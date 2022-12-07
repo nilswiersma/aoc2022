e031e69bd1cca060725eeeacf4adc474ba91d1ba
nils <Nils>
2022-12-07 08:48:04 +0100

    getinput

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+export AOC_COOKIE="session=XXX"
+python3 getinput.py X
+```
---
+++ getinput.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import requests
+from pathlib import Path
+
+day = int(sys.argv[1])
+
+url = f'https://adventofcode.com/2022/day/{day}/input'
+
+resp = requests.get(url, headers={'Cookie': os.environ['AOC_COOKIE']})
+
+assert resp.status_code == 200, resp.status_code
+
+out_dir = Path(f'{day:03d}/data')
+out_dir.mkdir(parents=True, exist_ok=True)
+
+with open(out_dir / 'input', 'w') as outf:
+    outf.write(resp.text)
+
