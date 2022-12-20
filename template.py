d275035e8cd4625aac0e71907a38d96445ff6911
nils <Nils>
2022-12-20 22:39:48 +0100

    update template

--- template.py
+++ template.py
@@ -1,4 +1,22 @@
 import sys
+import logging
+import argparse
 
-with open(sys.argv[1], 'r') as inp:
-    pass
+parser = argparse.ArgumentParser()
+parser.add_argument('inp', type=argparse.FileType('r'))
+parser.add_argument('-v', '--verbose', action='count', default=0)
+args = parser.parse_args()
+
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+logger.setLevel(logging.WARNING - args.verbose*10)
+
+with args.inp as inp:
+    l = inp.readline()
+    while l:
+        l = l.strip()
+        logger.debug(l)
+
+        pass
+
+        l = inp.readline()
